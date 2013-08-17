8e99f8c81cc7b17ef2d71e49443ba81026a69a98
Jemeyr <jmohr@uvic.ca>
2013-08-17 14:39:19 -0700

    Added mapping multiple keys to a value

--- src/input/Control.java
+++
@@ -1,7 +0,0 @@
-package input;
-
-public enum Control {
-	//TODO: Remove this and use a map, which allows "Jump" controls
-	DOWN, UP, LEFT, RIGHT, EXIT, SHIFT, CONTROL
-
-}
--- src/input/KeyboardController.java
+++ src/input/KeyboardController.java
@@ -2,18 +2,20 @@ package input;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.lwjgl.input.Keyboard;
 
 public class KeyboardController implements Controller{
 	
-	Map<String, Integer> controlMap;
+	Map<String, List<Integer>> controlMap;
 	
 	public KeyboardController(){
-		this.controlMap = new HashMap<String, Integer>();
-
+		this.controlMap = new HashMap<String, List<Integer>>();
+		
 		try{
 			BufferedReader br = new BufferedReader(new FileReader("temp/input.properties"));;
 		
@@ -23,29 +25,33 @@ public class KeyboardController implements Controller{
 			{
 				String[] tokens = line.split("=");
 				String property = tokens[0];
-				String value = tokens[1];
 				
-				int val;
+				String[] values = tokens[1].split(",");
+				List<Integer> keyCodes = new ArrayList<Integer>();
+				for(String value : values){
 				
-				switch(value)
-				{
-				case "up": val = Keyboard.KEY_UP; break;
-				case "down": val = Keyboard.KEY_DOWN; break;
-				case "left": val = Keyboard.KEY_LEFT; break;
-				case "right": val = Keyboard.KEY_RIGHT; break;
-				case "left_shift": val = Keyboard.KEY_LSHIFT; break;
-				case "left_ctrl": val = Keyboard.KEY_LCONTROL; break;
-				case "right_shift": val = Keyboard.KEY_RSHIFT; break;
-				case "right_ctrl": val = Keyboard.KEY_RCONTROL; break;
-				case "space": val = Keyboard.KEY_SPACE; break;
-				case "escape": val = Keyboard.KEY_ESCAPE; break;
-				default:
-					System.out.println("Error parsing " + value + " from properties file");
-					br.close();
-					return;
+					int val;
+					
+					switch(value)
+					{
+					case "up": val = Keyboard.KEY_UP; break;
+					case "down": val = Keyboard.KEY_DOWN; break;
+					case "left": val = Keyboard.KEY_LEFT; break;
+					case "right": val = Keyboard.KEY_RIGHT; break;
+					case "left_shift": val = Keyboard.KEY_LSHIFT; break;
+					case "left_control": val = Keyboard.KEY_LCONTROL; break;
+					case "right_shift": val = Keyboard.KEY_RSHIFT; break;
+					case "right_control": val = Keyboard.KEY_RCONTROL; break;
+					case "space": val = Keyboard.KEY_SPACE; break;
+					case "escape": val = Keyboard.KEY_ESCAPE; break;
+					default:
+						System.out.println("Error parsing " + value + " from properties file");
+						br.close();
+						return;
+					}
+					keyCodes.add(val);
 				}
-				
-				controlMap.put(property, val);	
+				controlMap.put(property, keyCodes);	
 			}
 			
 			br.close();
@@ -58,13 +64,20 @@ public class KeyboardController implements Controller{
 	}
 
 	@Override
-	public boolean isPressed(String key) {
-		if(controlMap.containsKey(key))
+	public boolean isPressed(String mapKey) {
+		if(controlMap.containsKey(mapKey))
 		{
-			return Keyboard.isKeyDown(controlMap.get(key));
+			for(Integer key : controlMap.get(mapKey)){
+				if(Keyboard.isKeyDown(key))
+				{
+					return true;
+				}
+			}
+			
+			return false;
 		}
 		
-		System.out.println("Error, invalid key " + key + " requested, not in map");
+		System.out.println("Error, invalid key " + mapKey + " requested, not in map");
 		return false;
 	}
 	
